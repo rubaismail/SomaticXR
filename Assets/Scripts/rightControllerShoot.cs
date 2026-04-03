c9c4585ccb6907bf689a364f572297c1abd51ec1
Josh Sawyer <jawshs08@gmail.com>
2026-04-03 15:19:32 -0400

    add projectiles that shoot from controllers

---
+++ Assets/Scripts/rightControllerShoot.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class rightControllerShoot : MonoBehaviour
+{
+    public GameObject prefab;
+    public float spawnSpeed = 5;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (OVRInput.GetDown(OVRInput.Button.SecondaryIndexTrigger))
+        {
+            GameObject spawnedBall = Instantiate(prefab, transform.position, Quaternion.identity); // Quaternion.identity means no rotation
+            Rigidbody spawnedBallRB = spawnedBall.GetComponent<Rigidbody>();
+            spawnedBallRB.linearVelocity = transform.forward * spawnSpeed;
+        }
+    }
+}
